6d4e190433a519f4e1f289cb7de31944f338c333
Mark Pitblado <86988982+arcticFox-git@users.noreply.github.com>
2023-10-17 23:06:28 -0700

    Adjust 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,27 +1,73 @@
 import Link from '@/components/Link'
+import { ChevronRightIcon } from '@heroicons/react/20/solid'
+import { BriefcaseIcon, UserCircleIcon, LockClosedIcon, RssIcon } from '@heroicons/react/24/solid'
+
+const links = [
+  {
+    name: 'Blog',
+    href: '/blog',
+    description: 'The main blog of this site.',
+    icon: RssIcon,
+  },
+  { name: 'About', href: '/about', description: 'A little bit about me.', icon: UserCircleIcon },
+  {
+    name: 'Projects',
+    href: '/projects',
+    description: 'Small projects that I am working on.',
+    icon: BriefcaseIcon,
+  },
+  {
+    name: 'Privacy',
+    href: '/privacy',
+    description: 'Privacy details for this site.',
+    icon: LockClosedIcon,
+  },
+]
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
-      <div className="space-x-2 pb-8 pt-6 md:space-y-5">
-        <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
-          404
-        </h1>
-      </div>
-      <div className="max-w-md">
-        <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-          Sorry I don't have this page.
-        </p>
-        <p className="mb-8">
-          But don't worry, you can find plenty of other things on the homepage.
-        </p>
-        <Link
-          href="/"
-          className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500"
-        >
-          Back to homepage
-        </Link>
-      </div>
-    </div>
+    <>
+      <main className="mx-auto w-full max-w-7xl px-6 pb-16 sm:pb-24 lg:px-8">
+        <div className="mx-auto mt-20 max-w-2xl text-center sm:mt-24">
+          <p className="text-base font-semibold leading-8 dark:text-lime-200">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
+            This page does not exist
+          </h1>
+          <p className="mt-4 text-base leading-7  sm:mt-6 sm:text-lg sm:leading-8">
+            Sorry, I don't have the page you are looking for. Below are the categories of the site.
+          </p>
+        </div>
+        <div className="mx-auto mt-6 flow-root max-w-lg sm:mt-20">
+          <h2 className="sr-only">Popular pages</h2>
+          <ul role="list" className="-mt-6 divide-y divide-gray-900/5 border-b border-gray-900/5">
+            {links.map((link, linkIdx) => (
+              <li key={linkIdx} className="relative flex gap-x-6 py-6">
+                <div className="flex h-10 w-10 flex-none items-center justify-center rounded-lg shadow-sm ring-1 ring-gray-900/10">
+                  <link.icon className="h-6 w-6 dark:text-lime-200" aria-hidden="true" />
+                </div>
+                <div className="flex-auto">
+                  <h3 className="text-sm font-semibold leading-6">
+                    <a href={link.href}>
+                      <span className="absolute inset-0" aria-hidden="true" />
+                      {link.name}
+                    </a>
+                  </h3>
+                  <p className="mt-2 text-sm leading-6">{link.description}</p>
+                </div>
+                <div className="flex-none self-center">
+                  <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+                </div>
+              </li>
+            ))}
+          </ul>
+          <div className="mt-10 flex justify-center">
+            <a href="/" className="text-sm font-semibold leading-6 dark:text-lime-200">
+              <span aria-hidden="true">&larr;</span>
+              Back to home
+            </a>
+          </div>
+        </div>
+      </main>
+    </>
   )
 }
