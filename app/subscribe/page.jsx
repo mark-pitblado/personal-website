1ced6ce8a4e024d3309b17fe2e91858106cd2ef3
Mark Pitblado <86988982+arcticFox-git@users.noreply.github.com>
2023-10-17 22:23:07 -0700

    Add subscribe page to root

---
+++ app/subscribe/page.jsx
@@ -0,0 +1,97 @@
+export default function Page() {
+    return (
+      <>
+        <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-md">
+            <img
+              className="mx-auto h-10 w-auto"
+              src="/static/images/buttondown.png"
+              alt="Your Company"
+            />
+            <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight">
+              Subscribe to my newsletter
+            </h2>
+          </div>
+  
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+            <div className="bg-white dark:bg-gray-900 px-6 py-12 shadow sm:rounded-lg sm:px-12">
+              <form className="space-y-6"   action="https://buttondown.email/api/emails/embed-subscribe/mark-pitblado"
+  method="post"
+  target="popupwindow"
+  onsubmit="window.open('https://buttondown.email/mark-pitblado', 'popupwindow')">
+                <div>
+                  <label htmlFor="bd-email" className="block text-sm font-medium leading-6">
+                    Email address
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="bd-email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+  
+  
+                <div>
+                  <button
+                    type="submit"
+                    value="Subscribe" 
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white dark:bg-lime-200 dark:text-black shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Subscribe
+                  </button>
+                </div>
+              </form>
+  
+              <div>
+                <div className="relative mt-10">
+                  <div className="absolute inset-0 flex items-center" aria-hidden="true">
+                    <div className="w-full border-t border-gray-200" />
+                  </div>
+                  <div className="relative flex justify-center text-sm font-medium leading-6">
+                    <span className="bg-white px-6 text-gray-900">Or follow the blog with</span>
+                  </div>
+                </div>
+  
+                <div className="mt-6 grid grid-cols-2 gap-4">
+                  <a
+                    href="/feed.xml"
+                    className="flex w-full items-center justify-center gap-3 rounded-md dark:bg-lime-200 px-3 py-1.5 text-white dark:text-black bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" fill="currentColor" viewBox="0 0 448 512"><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM96 136c0-13.3 10.7-24 24-24c137 0 248 111 248 248c0 13.3-10.7 24-24 24s-24-10.7-24-24c0-110.5-89.5-200-200-200c-13.3 0-24-10.7-24-24zm0 96c0-13.3 10.7-24 24-24c83.9 0 152 68.1 152 152c0 13.3-10.7 24-24 24s-24-10.7-24-24c0-57.4-46.6-104-104-104c-13.3 0-24-10.7-24-24zm0 120a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg>
+                    <span className="text-sm font-semibold leading-6">RSS</span>
+                  </a>
+  
+                  <a
+                    href="https://github.com/arcticFox-git"
+                    className="flex w-full items-center justify-center gap-3 rounded-md bg-[#24292F] px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F]"
+                  >
+                    <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                      <path
+                        fillRule="evenodd"
+                        d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <span className="text-sm font-semibold leading-6">GitHub</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+  
+            <p className="mt-10 text-center text-sm">
+              Newsletter powered by{' '}
+              <a href="https://buttondown.email/refer/mark-pitblado" target="_blank" className="font-semibold leading-6 text-indigo-500 hover:text-indigo-600 dark:text-lime-200 dark:hover:text-lime-300">
+                Buttondown
+              </a>
+            </p>
+          </div>
+        </div>
+      </>
+    )
+  }
+  
