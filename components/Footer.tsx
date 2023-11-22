3f98d696def12ed5083040dfaa72c6dc811edd75
Mark Pitblado <86988982+arcticFox-git@users.noreply.github.com>
2023-11-22 08:57:57 -0800

    Adjust footer after last commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,6 @@ export default function Footer() {
         <div className="mb-3 flex space-x-4">
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
           <SocialIcon kind="github" href={siteMetadata.github} size={6} />
-          <a rel="me" href="https://fosstodon.org/@markpitblado"></a>
           <SocialIcon kind="mastodon" href={siteMetadata.mastodon} size={6} />
           <SocialIcon kind="plausible" href={siteMetadata.plausible} size={6} />
           <SocialIcon kind="orcid" href={siteMetadata.orcid} size={6} />
@@ -19,7 +18,7 @@ export default function Footer() {
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>
             Except where otherwise noted, content on this site is licensed under a Creative Commons
-            Attribution 4.0 International license.
+            Attribution 4.0 International license. <a rel="me" href="https://fosstodon.org/@markpitblado"></a>
           </div>
         </div>
       </div>
