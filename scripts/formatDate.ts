7865fedd87e2cb8d163dc00b631194131d529540
Mark Pitblado <86988982+arcticFox-git@users.noreply.github.com>
2023-12-14 20:51:54 -0800

    Format with prettier

--- scripts/formatDate.ts
+++ scripts/formatDate.ts
@@ -1,10 +1,10 @@
 export const formatDate = (date: string, locale = 'en-US') => {
-    const options: Intl.DateTimeFormatOptions = {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-    }
-    const now = new Date(date).toLocaleDateString(locale, options)
-  
-    return now
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
   }
+  const now = new Date(date).toLocaleDateString(locale, options)
+
+  return now
+}
