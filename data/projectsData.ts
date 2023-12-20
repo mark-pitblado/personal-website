3804d64e4049fcab3638cd3b88bd3eda4e0de030
Mark Pitblado <86988982+arcticFox-git@users.noreply.github.com>
2023-12-20 09:00:13 -0800

    Add never monetize

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,22 +1,32 @@
 const projectsData = [
   {
     title: 'Understanding a Fracking Site',
-    description: 'A simple react site to better understand hydraulic fracturing in British Columbia.',
+    description:
+      'A simple react site to better understand hydraulic fracturing in British Columbia.',
     imgSrc: '/static/images/understanding-a-fracking-site-screenshot.png',
     href: 'https://showcase2.markpitblado.me',
   },
   {
     title: 'Hazard x Outrage',
-    description: 'Another react app, this time as a single page, to understand the Hazard x Outrage risk communication framework in public health.',
+    description:
+      'Another react app, this time as a single page, to understand the Hazard x Outrage risk communication framework in public health.',
     imgSrc: '/static/images/hazard-x-outrage-screenshot.png',
     href: 'https://showcase1.markpitblado.me',
   },
-  { 
+  {
     title: 'Biodiversity Informatics',
-    description: 'A work in progress, but hope to build a suite to help with biodiversity informatics.',
+    description:
+      'A work in progress, but hope to build a suite to help with biodiversity informatics.',
     imgSrc: '/static/images/biodiversity-informatics-screenshot.png',
-    href: 'https://biodiversityinformatics.ca'
-  }
+    href: 'https://biodiversityinformatics.ca',
+  },
+  {
+    title: 'Never Monetize',
+    description:
+      'A jekyll site that can be linked to from other websites to communicate monetization intent with users',
+    imgSrc: '/static/images/never-monetize-screenshot.png',
+    href: 'https://nevermonetize.com',
+  },
 ]
 
 export default projectsData
