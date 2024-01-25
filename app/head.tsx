c829400b43ca11f231a92880136995294cef8bb1
Mark Pitblado <86988982+arcticFox-git@users.noreply.github.com>
2024-01-24 22:26:25 -0800

    add additional favicons and change main one

--- app/head.tsx
+++ app/head.tsx
@@ -2,6 +2,9 @@ export default function Head() {
   return (
     <>
       <link rel="icon" href="/static/favicons/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
