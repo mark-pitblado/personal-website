c5eb7e104a81e6505fb706a70b78579f938b3cee
Mark Pitblado <mark@pitblado.me>
2024-02-25 21:45:36 -0800

    add notes to header

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
+  { href: 'https://notes.markpitblado.me', title: 'Notes' },
   { href: '/about', title: 'About' },
   { href: '/privacy', title: 'Privacy' },
 ]
