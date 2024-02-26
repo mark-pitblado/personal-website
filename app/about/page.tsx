d850177564fb29fa2bd581440cf75cca3bbdc841
Mark Pitblado <mark@pitblado.me>
2024-02-25 19:47:02 -0800

    format with prettier

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,9 +13,14 @@ export default function Page() {
           </h1>
         </div>
         <div className="text-text prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2">
-
-          Hi, I'm Mark! Thanks for stopping by my site. Here you will find a little bit about me, some posts that I have written, and links to other websites or services that I have a presence on. If you with to get in touch, please use the email logo in the footer, and I'll respond as soon as I can.
-          I grew up loving to tinker with things, and never really stopped. If I were to describe what I do, I would summarize it as "using data to help solve problems for the public good". In my free time I love to read, and explore new and emerging open-source software. Currently, I a</div>
+          Hi, I'm Mark! Thanks for stopping by my site. Here you will find a little bit about me,
+          some posts that I have written, and links to other websites or services that I have a
+          presence on. If you with to get in touch, please use the email logo in the footer, and
+          I'll respond as soon as I can. I grew up loving to tinker with things, and never really
+          stopped. If I were to describe what I do, I would summarize it as "using data to help
+          solve problems for the public good". In my free time I love to read, and explore new and
+          emerging open-source software. Currently, I a
+        </div>
         <Link href="/about/this-site">
           <button className="rounded-md bg-green px-3 py-2 text-sm font-semibold text-base shadow-sm hover:bg-blue">
             {' '}
@@ -26,12 +31,3 @@ export default function Page() {
     </>
   )
 }
-
-
-
-
-
-
-
-
-
--- layouts/PostSimple.tsx
+++ layouts/PostSimple.tsx
@@ -40,7 +40,9 @@ export default function PostLayout({ content, next, prev, children }: LayoutProp
           </header>
           <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:divide-y-0">
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
-              <div className="prose text-text max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
+              <div className="prose text-text max-w-none pb-8 pt-10 dark:prose-invert">
+                {children}
+              </div>
             </div>
             <footer>
               <div className="flex flex-col text-sm font-medium sm:flex-row sm:justify-between sm:text-base">
